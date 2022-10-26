cd693bd9077728b747b9c40f2f2ed9a9cc1984cc
DianaSagoyan <dianasagoyan@yahoo.com>
2022-10-26 16:21:24 -0500

    Initial commit

---
+++ lab-06-orm/src/main/java/com/cydeo/lab06orm/Lab06OrmApplication.java
@@ -0,0 +1,11 @@
+package com.cydeo.lab06orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab06OrmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Lab06OrmApplication.class, args);
+    }
+}
